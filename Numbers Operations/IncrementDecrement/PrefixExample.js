c0aeae439e91a8e3e505bfef18335655c2d25a0c
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 03:01:04 +1000

    Update PrefixExample.js

--- Numbers Operations/IncrementDecrement/PrefixExample.js
+++ Numbers Operations/IncrementDecrement/PrefixExample.js
@@ -1,9 +1,18 @@
+
+/* Prefix increment
+    Decrement (a is now incremented by 1; now a = 4)
+    Assignment (y stores value of a = 4; now y = 4)
+*/
 let a = 3;
 const b = ++a;
 
 console.log(`a:${a}, b:${b}`);
 // expected output: "a:4, b:4"
 
+/* Prefix decrement
+    Decrement (a is now decremented by 1; now a = 2)
+    Assignment (y stores value of a = 2; now y = 2)
+*/
 let a = 3;
 const b = --a;
 
