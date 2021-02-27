b44be75505c682e61f0cd407d8357029905d5e6a
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 02:59:14 +1000

    Update PostfixExample.js

--- Numbers Operations/IncrementDecrement/PostfixExample.js
+++ Numbers Operations/IncrementDecrement/PostfixExample.js
@@ -8,11 +8,11 @@ const y = x++;
 console.log(`x:${x}, y:${y}`);
 // expected output: "x:4, y:3"
 
+
 /* Postfix decrement
     Assignment (y stores previous value of x = 3; now y = 3)
     Decrement (x is now decremented by 1; now x = 2 and does not affect value of y)
 */
-
 let x = 3;
 const y = x--;
 
