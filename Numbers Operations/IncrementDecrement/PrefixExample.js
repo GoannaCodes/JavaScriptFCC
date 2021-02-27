2bbe8ae7b24241360f48688a0a358ea160701a51
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 02:58:54 +1000

    Create PrefixExample.js

---
+++ Numbers Operations/IncrementDecrement/PrefixExample.js
@@ -0,0 +1,11 @@
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// expected output: "a:4, b:4"
+
+let a = 3;
+const b = --a;
+
+console.log(`a:${a}, b:${b}`);
+// expected output: "a:2, b:2"
