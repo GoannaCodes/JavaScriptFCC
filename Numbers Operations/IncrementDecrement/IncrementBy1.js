54e2143150c7900b3d9b9a7a84c17a2c501da8a5
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 02:55:24 +1000

    Create IncrementBy1.js

---
+++ Numbers Operations/IncrementDecrement/IncrementBy1.js
@@ -0,0 +1,10 @@
+/* Increment using i++;
+    Below is the same as doing:
+      increment = increment + 1;
+    Which is still 99 + 1
+*/
+
+var increment = 99;
+increment++; // now equal to 100
+
+console.log("Increment value was 99 and is now ${increment}");
