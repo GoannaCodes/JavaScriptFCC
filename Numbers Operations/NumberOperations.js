d0ee26cb6352d064deee063eda139e37b305d5be
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 02:15:02 +1000

    Create NumberOperations.js

---
+++ Numbers Operations/NumberOperations.js
@@ -0,0 +1,11 @@
+//Addition
+add = 5 + 10; //stores 15
+
+//Subtraction
+subtract = 12 - 6; //stores 6
+
+//Division
+divide = 16 / 2; //stores 8
+
+//Multiplication
+multiply = 13 * 13; //stores 169
