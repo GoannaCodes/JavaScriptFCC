211aa86273e115edc8b38d3267dda37027edc70e
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-02-28 02:25:29 +1000

    Create IncrementDecrement.js

---
+++ Numbers Operations/IncrementDecrement.js
@@ -0,0 +1,18 @@
+//Incrment using i++;
+var increment = 99;
+increment++; //now equal to 100
+
+/* Above is the same as doing:
+    increment = increment + 1;
+  Which is still 99 + 1
+*/
+
+
+//Decrement using i--;
+var decrement = 55; 
+decrement--; //now equal to 54
+
+/* Above is the same as doing:
+    decrement = decrement - 1;
+   Which is still 55 - 1
+*/
